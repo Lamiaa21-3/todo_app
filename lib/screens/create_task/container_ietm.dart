3b8454ec95a2010ba84fc704dcf2fb8f0f54b358
Lamiaa21-3 <lamiaagamal213@gmail.com>
2023-12-03 13:58:38 +0200

    update ui

--- lib/screens/create_task/container_ietm.dart
+++ lib/screens/create_task/container_ietm.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../components/text_main.dart';
 import '../../components/text_middle.dart';
 import '../../components/text_small.dart';
+import '../create_task_details/container_ietm_column.dart';
 
 
 class ContainerIetm extends StatelessWidget {
@@ -13,53 +14,81 @@ class ContainerIetm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      child: Padding(
-        padding: const EdgeInsets.only(left: 8,top: 8),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextMain(
-                string: '$mainText',
-              ),
-              Row(
-                children: [
-                  Icon(Icons.timer_outlined),
-                  SizedBox(
-                    width: 9,
+    return  GestureDetector(
+      onTap: (){
+        showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return  Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ContainerIetmColumn(text: 'Task Completed ', color:  Color(0xFF8875FF),onTapContaoner: (){},),
+                      ContainerIetmColumn(text: 'Delete Task', color:  Color(0xFFE57373),onTapContaoner: (){}),
+                      ContainerIetmColumn(text: 'Cancel ', color:  Color(0xFF8875FF),onTapContaoner: (){
+                        Navigator.pop(context);
+                      }),
+                    ],
                   ),
-                  TextDate(
-                    text: '09:33 PM - 09:48 PM',
-                  ),
-                  SizedBox(
-                    width: 90,
-                  ),
-                  Container(
-                    width: 2,
-                    height: 60,
-                    color: Color(0xFFC4C4C4),
-                  ),
-                  SizedBox(width: 3,),
-                  new RotatedBox(
-                      quarterTurns: 3,
-                      child: new   Text(
-                        'TODO',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontFamily: 'Lato',
-                            fontSize: 16),
-                      )
-                  )
-                ],
-              ),
-              TextMiddle(text: '$subText'),
-            ]),
-      ),
-      width: 375,
-      height: 128,
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(16),
+                ),
+                width: 375,
+                height: 240,
+                decoration: BoxDecoration(
+                    color: Color(0xFF424242)
+                ),
+              );
+            });
+      },
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 8,top: 8),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextMain(
+                  string: '$mainText',
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.timer_outlined),
+                    SizedBox(
+                      width: 9,
+                    ),
+                    TextDate(
+                      text: '09:33 PM - 09:48 PM',
+                    ),
+                    SizedBox(
+                      width: 90,
+                    ),
+                    Container(
+                      width: 2,
+                      height: 60,
+                      color: Color(0xFFC4C4C4),
+                    ),
+                    SizedBox(width: 3,),
+                    new RotatedBox(
+                        quarterTurns: 3,
+                        child: new   Text(
+                          'TODO',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontFamily: 'Lato',
+                              fontSize: 16),
+                        )
+                    )
+                  ],
+                ),
+                TextMiddle(text: '$subText'),
+              ]),
+        ),
+        width: 375,
+        height: 128,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(16),
+        ),
       ),
     );
   }
